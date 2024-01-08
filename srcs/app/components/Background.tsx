4d11d006ca364f8f7e96bba695fb0fbbb45a77cc
Ekhorn <koen@kschellingerhout.nl>
2024-01-08 06:39:38 +0100

    feat(app): add backgound configuration support

--- srcs/app/components/Background.tsx
+++ srcs/app/components/Background.tsx
@@ -1,15 +1,42 @@
+import { createMemo } from 'solid-js';
+
+import { useViewport } from './ViewportProvider.jsx';
+
 export function Background() {
+  const { absoluteViewportPosition, scalar } = useViewport();
+
+  const dist = 50;
+  const offsetX = createMemo(
+    () => (-absoluteViewportPosition().x % dist) * scalar(),
+  );
+  const offsetY = createMemo(
+    () => (absoluteViewportPosition().y % dist) * scalar(),
+  );
+  const size = createMemo(() =>
+    scalar() > 1
+      ? (dist * scalar()) /
+        (Math.trunc((dist * Math.log2(scalar())) / dist / 2) + 1)
+      : dist * scalar(),
+  );
   return (
     <svg class="absolute z-0 h-full w-full">
       <pattern
         id="background"
-        width="13"
-        height="13"
+        x={offsetX()}
+        y={offsetY()}
+        width={size()}
+        height={size()}
         patternUnits="userSpaceOnUse"
       >
-        <circle cx="0.7" cy="0.7" r="0.7" fill="#363636" />
+        <circle cx="1.7" cy="1.7" r="1.7" fill="black" />
       </pattern>
-      <rect x="0" y="0" fill="url(#background)" class="h-full w-full" />
+      <rect
+        x="0"
+        y="0"
+        fill="url(#background)"
+        class="h-full w-full"
+        shape-rendering="optimizeSpeed"
+      />
     </svg>
   );
 }
