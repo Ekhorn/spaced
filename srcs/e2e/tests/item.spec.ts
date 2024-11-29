a9c1210073070e1b958af16b2be013771bf035ba
Ekhorn <koen@kschellingerhout.nl>
2024-11-29 16:20:35 +0100

    test: collaboration feature

--- srcs/e2e/tests/item.spec.ts
+++ srcs/e2e/tests/item.spec.ts
@@ -1,6 +1,8 @@
 import { type Page, test, expect } from '@playwright/test';
 import { type Item } from 'types';
 
+import { toEndOfLine } from '../utils/editor.js';
+
 const addItem = async (page: Page, x: number, y: number, width: number) => {
   await page.waitForLoadState('domcontentloaded');
   await page.evaluate(
@@ -325,5 +327,128 @@ test.describe('Markdown item', () => {
         );
       });
     }
+  });
+});
+
+test.describe('Item collaboration', () => {
+  test.beforeEach(async ({ context, page }) => {
+    await context.grantPermissions(['clipboard-write', 'clipboard-read']);
+    await page.goto('/');
+    await page.waitForLoadState('domcontentloaded');
+  });
+
+  test('should collaborate', async ({ browser, page: page1 }) => {
+    await page1.getByTitle('Create Item').click();
+
+    const editor1 = page1.locator('[data-slate-editor]');
+
+    await editor1.dblclick();
+    await editor1.pressSequentially('Collaborating...');
+    await expect(editor1).toContainText('Collaborating...');
+
+    await page1.getByTitle('Share').click();
+
+    const user1 = 'User 1';
+    await page1.locator("input[name='username']").pressSequentially(user1);
+    await page1.evaluate(() => {
+      const input = document.querySelector(
+        "input[name='color']",
+      ) as HTMLInputElement;
+      if (input) {
+        input.value = '#27D835';
+        input.dispatchEvent(new Event('change', { bubbles: true }));
+      }
+    });
+
+    await page1.click("button[type='submit']");
+
+    const code = await page1.evaluate(
+      async () => await window.navigator.clipboard.readText(),
+    );
+    expect(code).toBeTruthy();
+
+    const context2 = await browser.newContext();
+    const page2 = await context2.newPage();
+    await page2.goto('/');
+
+    await page2.getByPlaceholder('Enter code').fill(code);
+    await page2.getByPlaceholder('Enter code').press('Enter');
+
+    const user2 = 'User 2';
+    await page2.locator("input[name='username']").pressSequentially(user2);
+    await page2.evaluate(() => {
+      const input = document.querySelector(
+        "input[name='color']",
+      ) as HTMLInputElement;
+      if (input) {
+        input.value = '#F44678';
+        input.dispatchEvent(new Event('change', { bubbles: true }));
+      }
+    });
+
+    const editor2 = page2.locator('[data-slate-editor]');
+
+    await expect(editor2).toContainText('Collaborating...');
+
+    await editor2.dblclick();
+    await editor2.click();
+    await toEndOfLine(editor1, 0);
+    await editor2.pressSequentially(', yes we are!');
+
+    const text = await editor1.textContent();
+    // TODO: handle invalid cursors remove normalize string remove BOM unicode chars etc.
+    expect(text?.replace('\uFEFF', '')).toBe(
+      `Collaborating..., yes we are${user2}!`,
+    );
+    // TODO: fix cursor position to be in the correct location
+    await expect(editor2).toContainText(`Collaborating..., yes we are!`);
+
+    await editor1.dblclick();
+    await toEndOfLine(editor1, 0);
+    await editor1.press('Enter');
+    await editor1.pressSequentially(':)');
+
+    // TODO: create test fixture checking multiline editor content
+    await expect(editor1.locator(`p`)).toContainText([
+      `Collaborating..., yes we are!${user2}`,
+      `:)`,
+    ]);
+    // TODO: fix cursor position to be in the correct location
+    for (const [i, expected] of [
+      `Collaborating..., yes we are!`,
+      `:${user1})`,
+    ].entries()) {
+      const text = await editor2.locator(`p`).nth(i).textContent();
+      expect(text?.replace('\uFEFF', '')).toBe(expected);
+    }
+
+    await test.step('disconnect editor', async () => {
+      await page1.getByTitle('Stop Sharing').click();
+      await expect(editor1.locator(`p`)).toContainText([
+        `Collaborating..., yes we are!`,
+        `:)`,
+      ]);
+      // TODO: FIX
+      // await expect(editor2.locator(`p`)).toContainText([
+      //   `Collaborating..., yes we are!`,
+      //   `:)`,
+      // ]);
+    });
+  });
+
+  test('should cancel before sharing', async ({ page }) => {
+    await page.getByTitle('Create Item').click();
+
+    const editor = page.locator('[data-slate-editor]');
+
+    await editor.dblclick();
+    await editor.pressSequentially('Collaborating...');
+    await expect(editor).toContainText('Collaborating...');
+
+    await page.getByTitle('Share').click();
+    await page.getByTitle('Cancel').click();
+    // TODO: check websocket connection?
+    await expect(page.getByTitle('Cancel')).toBeHidden();
+    await expect(page.getByTitle('Share')).toBeVisible();
   });
 });
