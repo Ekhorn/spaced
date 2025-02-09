0b2c9c4e00368ed762458b94bacafcb789fee1dd
Ekhorn <koen@kschellingerhout.nl>
2025-02-09 23:15:21 +0100

    fix(nix): switch to unstable playwright browsers in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 let
   pkgs = import <nixpkgs> { };
+  unstable = import (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") { };
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
@@ -17,7 +18,7 @@ pkgs.mkShell {
     openssl
   ];
   nativeBuildInputs = with pkgs; [
-    playwright-driver.browsers
+    unstable.playwright.browsers
     pkg-config
     gobject-introspection
     cargo
@@ -29,7 +30,7 @@ pkgs.mkShell {
   shellHook =
     ''
       export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-      export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+      export PLAYWRIGHT_BROWSERS_PATH=${unstable.playwright.browsers}
       export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
     '';
 }
